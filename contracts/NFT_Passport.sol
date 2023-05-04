309d16362677aa8a924dcdd049520ada96c01aeb
Artem Sidnev <proektyit@gmail.com>
2023-05-04 19:23:19 +0300

    first commit

---
+++ contracts/NFT_Passport.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import "./interfaces/IVerification.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+
+contract NFT_Passport is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable, OwnableUpgradeable {
+    event Attest(address indexed to, uint256 indexed  tokenId);
+    event Revoke(address indexed to, uint256 indexed  tokenId);
+
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+
+    CountersUpgradeable.Counter private _tokenIdCounter;
+    IVerification public verificationContract;
+
+    // constructor(address _verificationContract) ERC721("YourSoulboundPassport", "PSPRT") {
+    //     verificationContract = IVerification(_verificationContract);
+    // }
+
+    function initialize(address _verificationContract) initializer public {
+        __ERC721_init("YourSoulboundPassport", "PSPRT");
+        __ERC721URIStorage_init();
+        __Ownable_init();
+        verificationContract = IVerification(_verificationContract);
+    }
+    function safeMint(address to, string memory uri) public onlyOwner {
+        require(verificationContract.verifiedUsers(to), "Can't mint passport to NOT verified users!");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+    function burn(uint256 tokenId) external {
+        require(ownerOf(tokenId) == msg.sender, "Only owner of the token can burn it");
+        _burn(tokenId);
+    }
+    function revoke(uint256 tokenId) onlyOwner external {
+        _burn(tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+    function _beforeTokenTransfer(address from, address to, uint256 firstTokenId, uint256 batchSize) internal override virtual {
+        require(from == address(0) || to == address(0), "only burning & minting");
+    }
+    function _afterTokenTransfer(address from, address to, uint256 firstTokenId, uint256 batchSize) internal override virtual {
+        if(from == address(0)){
+            emit Attest(to, firstTokenId);
+        } else if (to == address(0)){
+            emit Revoke(to, firstTokenId);
+        }
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721Upgradeable, ERC721URIStorageUpgradeable) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
+
+// TESTING 
+// 1) mint
+// 2) try to transfer (it must revert)
+// 3) burn must be okay. it will burn tokens
---
+++ test/NFT_Passport.test.js
@@ -0,0 +1,42 @@
+const { AddressZero } = require("@ethersproject/constants");
+const { expect } = require("chai");
+const { ethers, upgrades } = require("hardhat");
+
+describe("NFT_Passport", function(){
+    let passport;
+    let verificationContract;
+    let owner;
+    let user;
+
+    beforeEach(async function(){
+        [owner, user] = await ethers.getSigners();
+
+        const VerificationContract = await ethers.getContractFactory("Verification");
+        verificationContract = await VerificationContract.deploy();
+        await verificationContract.deployed()
+
+        const NFT_Passport = await ethers.getContractFactory("NFT_Passport");
+        passport = await upgrades.deployProxy(NFT_Passport, [verificationContract.address])
+        await passport.deployed()
+    });
+
+    it("mint", async function(){
+        const signer = owner;
+        const to = user.address;
+        const message = "Verify your identity to get NFT passport";
+        const nonce = 123;
+
+        const hash = await verificationContract.getMessageHash(to, message, nonce);
+        const sig = await signer.signMessage(ethers.utils.arrayify(hash))
+        const ethHash = await verificationContract.getEthSignedMessageHash(hash)
+        console.log("signer          ", signer.address)
+        console.log("recovered signer", await verificationContract.recoverSigner(ethHash, sig))
+    
+        // Correct signature and message returns true
+        await verificationContract.verify(signer.address, to, message, nonce, sig);
+        
+       // expect(await verificationContract.verify(signer.address, to, message, nonce, sig)).to.be.equal(false)
+        await expect(await passport.safeMint(user.address, "uri")).to.emit(passport, "Transfer");
+
+    })
+})
